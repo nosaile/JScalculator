681bea6da31ffcffb18a8bba3ce88a9dedb46560
Chris Eliason <98778355+nosaile@users.noreply.github.com>
2022-08-17 12:16:04 -0500

    calculator in js html css

---
+++ main.js
@@ -0,0 +1,34 @@
+"use strict";
+
+let display = document.getElementById('display');
+let buttons = Array.from(document.getElementsByClassName('button'));
+console.log(buttons)
+console.log(display)
+
+
+buttonEvents()
+
+function buttonEvents() {
+    buttons.map(button => {
+        button.addEventListener('click', (e) => {
+            switch (e.target.innerText) {
+                case("ac"):
+                    display.innerText = '';
+                    break;
+                case("del"):
+                    display.innerText = display.innerText.slice(0, -1);
+                    break;
+                case('='):
+                    try {
+                        display.innerText = eval(display.innerText)
+                    } catch {
+                        display.innerText = "! error !";
+                    }
+                    break;
+                default:
+                    display.innerText += e.target.innerText;
+            }
+
+        })
+    })
+}
